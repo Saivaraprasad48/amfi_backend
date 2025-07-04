fbb85d8a183c70ca8560daffb542ffdeaa7e2e5d
Sai Vara Prasad Degala <93783719+Saivaraprasad48@users.noreply.github.com>
2025-07-04 21:28:15 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sai_vara_db:Dsvp%404904@cluster0.vf8la7o.mongodb.net/amfi",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error("Error connecting to DB:", error);
+  }
+}
+
+connectToDatabase();
+
+const navSchema = new mongoose.Schema(
+  {
+    scheme: String,
+    nav: String,
+    date: String,
+    lastRefreshed: String,
+  },
+  { timestamps: true }
+);
+
+const NAVModel = mongoose.model("NAV", navSchema);
+
+module.exports = {
+  NAVModel,
+};
